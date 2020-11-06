52c429d56db17f9f50f0253167804335762263b5
Kimberly Lin <kimberlylin2014@gmail.com>
2020-11-05 21:17:29 -0800

    setup catch all route

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
                   return currentUser ? <SingleMoviePage {...props}/> :  <Redirect to='/login'/>
                 }} 
                 />
-                <Route component={NotFoundPage} />
+                <Route path='/*' component={NotFoundPage} />
             </Switch>
             <Footer />
           </Suspense>
